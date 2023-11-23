549373dd684a56e3a4f4fe4c5b55108b1c216e1b
Jessica Jiang <jessica.jiang@2degrees.nz>
2023-11-23 14:03:00 +1300

    added list for displaying variants

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://www.google.co.nz',
             inventory: 100,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
         }
     }
 })
