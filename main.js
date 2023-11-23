ec79c4a6207ba6e73ea3beb223cb13f09b8108af
Jessica Jiang <jessica.jiang@2degrees.nz>
2023-11-23 13:48:31 +1300

    added onSale boolean to data object that shows On Sale message if true

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.google.co.nz',
-            inventory: 100
+            inventory: 100,
+            onSale: true
         }
     }
 })
