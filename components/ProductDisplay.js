3b22d27b280b9d507a2caca99673fbb19f369fb3
Jessica Jiang <jessica.jiang@2degrees.nz>
2023-11-24 09:15:38 +1300

    moved code into components and made a prop to pass data into the component

---
+++ components/ProductDisplay.js
@@ -0,0 +1,85 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:
+        /*html*/
+        `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <!-- image goes here -->
+        <img :src="image" :class="[inStock ? '' : 'out-of-stock-img']">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock > 10">In Stock</p>
+        <p v-else-if="inStock <= 10 && inStock >0">Almost Sold Out!</p>
+        <p v-else>Out of Stock</p>
+        <p>{{ sale }}</p>
+        <p>Shipping: {{ shipping }}</p>
+        
+        <product-details :details="details"></product-details>
+
+        <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+          class="color-circle" :style="{ backgroundColor: variant.color }">
+        </div>
+        <button class="button" :class="{ disabledButton: !inStock }":disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
+        <button class="button" :class="{ disabledButton: cart == 0 }":disabled="cart == 0" @click="removeFromCart">Remove Item</button>
+      </div>
+    </div>
+  </div>`,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 5 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart++
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart--
+            }
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + this.product + ' is on sale!'
+            } else {
+                return ''
+            }
+        },
+        shipping() {
+            if (this.premium) {
+                return 'Free'
+            }
+            return 2.99
+        }
+    }
+
+})
